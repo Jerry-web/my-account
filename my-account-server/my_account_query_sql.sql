41a720e1603ba1f877e24b07e3b62002aed9664f
Jerry-web <13811268228@163.com>
2017-06-13 18:22:53 +0800

    sql commit

---
+++ my-account-server/my_account_query_sql.sql
@@ -0,0 +1,6 @@
+INSERT INTO `my_account_query_sql` VALUES (1, '2017-6-13 10:51:59', '生活用品', 345);
+INSERT INTO `my_account_query_sql` VALUES (2, '2017-6-12 10:53:20', '购物', 2593.5);
+INSERT INTO `my_account_query_sql` VALUES (7, '2017-6-1 00:00:00', '医疗', 460.25);
+INSERT INTO `my_account_query_sql` VALUES (3, '2017-6-1 10:53:38', '生活费', 3000);
+INSERT INTO `my_account_query_sql` VALUES (4, '2017-6-13 00:00:00', '其他', 871);
+
